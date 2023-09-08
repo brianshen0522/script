e36700f5c4c9c42b22651d8f26037a7cbcbd5b97
codessse <57928832+codessse@users.noreply.github.com>
2023-09-08 18:33:44 +0800

    Update ct.sh
    install

--- ct.sh
+++ ct.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-apt install vim ncdu lnav ssh sudo wget curl bat -y
+apt install -y vim ncdu lnav ssh sudo wget curl bat gpg tree git dnsutils cifs-utils \
+python3-pip
 adduser --disabled-password --gecos "" user
 echo user:" " | chpasswd
 adduser user sudo
