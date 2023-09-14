a4e56e6c0dd8db80339e8b5b3b5eb913b8a81bc4
codessse <57928832+codessse@users.noreply.github.com>
2023-09-14 13:26:05 +0800

    Update ct.sh
    update

--- ct.sh
+++ ct.sh
@@ -3,6 +3,13 @@ apt update
 apt upgrade -y
 apt install -y vim ncdu lnav ssh sudo wget curl bat gpg tree git htop dnsutils cifs-utils linux-headers-$(uname -r) \
 python3-pip
-adduser --disabled-password --gecos "" user
-echo user:" " | chpasswd
-adduser user sudo
+
+if ! id -u user >/dev/null 2>&1; then
+  adduser --disabled-password --gecos "" user
+  echo user:" " | chpasswd
+  adduser user sudo
+else
+  echo "user exists"
+fi
+
+
