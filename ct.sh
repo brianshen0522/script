2ffd09ca1f6a262904caaa1490da0c34f6537c01
codessse <57928832+codessse@users.noreply.github.com>
2023-09-21 18:59:02 +0800

    Update ct.sh
    update

--- ct.sh
+++ ct.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-apt install -y vim ncdu lnav ssh sudo wget curl bat gpg tree git htop neofetch dnsutils cifs-utils linux-headers-$(uname -r) \
+apt install -y vim ncdu lnav ssh sudo wget curl bat gpg tree git htop neofetch dnsutils cifs-utils \
 python3-pip
+
+apt install linux-headers-$(uname -r) 
 
 if ! id -u user >/dev/null 2>&1; then
   adduser --disabled-password --gecos "" user
