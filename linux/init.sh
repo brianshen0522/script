77f022b6da050d8ba246e584ecc35f0f8c8a32cd
codessse <57928832+codessse@users.noreply.github.com>
2023-10-20 13:25:34 +0800

    Update init.sh
    Update init.sh

--- linux/init.sh
+++ linux/init.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y vim ncdu lnav ssh sudo wget curl bat gpg tree git htop neofetch dnsutils cifs-utils tmux \
 python3-pip
+apt-get autoremove
 
 if ! id -u user >/dev/null 2>&1; then
   adduser --disabled-password --gecos "" user
