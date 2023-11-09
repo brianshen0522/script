6faab4db8acd8aebe63e5de96d157658c343df57
codessse <57928832+codessse@users.noreply.github.com>
2023-11-09 12:53:52 +0800

    Update docker_postgresql_upgrade.sh
    Update docker_postgresql_upgrade.sh

--- docker/docker_postgresql_upgrade.sh
+++ docker/docker_postgresql_upgrade.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-
 mkdir -p postgres-upgrade-testing
 cd postgres-upgrade-testing || exit
 OLD='12'
-NEW='15'
-DBname='testdb'                   #DBname can only be lowercase
-SQLFILE='./testDB_backup.sql'
+NEW='16'
+DBname='gitlab'                   #DBname can only be lowercase
+SQLFILE='/home/user/tmp/gitlab.sql'
 
 echo "Create Old PostgreSQL container..."
 docker run -dit --name postgres-upgrade-testing -e POSTGRES_PASSWORD=password -v "$PWD/$OLD/data":/var/lib/postgresql/data "postgres:$OLD"
-sleep 1
+sleep 3
 echo " "
 echo "Container Created"
 
 echo " "
-echo "Processing Old PostgreSQL..."
+echo "Create Old Database..."
 docker exec -i postgres-upgrade-testing psql -U postgres -c "CREATE DATABASE $DBname;"
+echo " "
+echo "Insert $DBname sql file"
 cat $SQLFILE | docker exec -i postgres-upgrade-testing psql -U postgres -d $DBname  &> /dev/null
+echo " "
+echo "Remove old Container"
 docker stop postgres-upgrade-testing
 docker rm postgres-upgrade-testing
 
@@ -28,13 +31,13 @@ docker run --rm -v "$PWD":/var/lib/postgresql "tianon/postgres-upgrade:$OLD-to-$
 echo " "
 echo "Processing New PostgreSQL..."
 docker run -dit --name postgres-upgrade-testing -e POSTGRES_PASSWORD=password -v "$PWD/$NEW/data":/var/lib/postgresql/data "postgres:$NEW"
-sleep 1
+sleep 3
 
 echo " "
 echo "Exporting New PostgreSQL file..."
 datetime=$(date +%Y-%m-%d_%H_%M_%S)
 docker exec -i postgres-upgrade-testing pg_dump -U postgres $DBname | sudo tee "../${DBname}_dump_${datetime}.sql" > /dev/null
-sleep 1
+sleep 3
 sudo chown "$USER":"$USER" "../${DBname}_dump_${datetime}.sql"
 cd ..
 
@@ -45,6 +48,7 @@ docker stop postgres-upgrade-testing
 docker rm postgres-upgrade-testing
 docker rmi "postgres:$OLD" "postgres:$NEW"
 sudo rm -fr postgres-upgrade-testing
+docker rmi "tianon/postgres-upgrade:$OLD-to-$NEW"
 unset OLD NEW DBname SQLFILE datetime
 
 echo " "
