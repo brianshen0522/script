51ad9fc52e9cd7640fac3ffc1fa88130fac7cc0c
codessse <57928832+codessse@users.noreply.github.com>
2023-11-22 14:39:03 +0800

    Update update.sh
    Update update.sh

--- linux/update.sh
+++ linux/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "sudo apt-get clean -y" > update.sh
-echo "sudo apt-get update -y" > update.sh
-echo "sudo apt-get upgrade -y" > update.sh
-echo "sudo apt-get autopurge -y" > update.sh
+echo "sudo apt-get update -y" >> update.sh
+echo "sudo apt-get upgrade -y" >> update.sh
+echo "sudo apt-get autopurge -y" >> update.sh
 
 chmod +x update.sh
 ./update.sh
