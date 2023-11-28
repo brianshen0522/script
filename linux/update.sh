b0297bdd3641b2fd61a6b28cb97f9501b60a4024
codessse <57928832+codessse@users.noreply.github.com>
2023-11-28 21:54:18 +0800

    Update update.sh
    Update update.sh

--- linux/update.sh
+++ linux/update.sh
@@ -5,4 +5,6 @@ echo "sudo apt-get upgrade -y" >> update.sh
 echo "sudo apt-get autopurge -y" >> update.sh
 
 chmod +x update.sh
+export DEBIAN_FRONTEND=noninteractive
 ./update.sh
+unset DEBIAN_FRONTEND
