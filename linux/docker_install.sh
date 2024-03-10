24738b5feeefe0b61d2923299518aca637e9ed99
brain9429 <44171953+brianshen0522@users.noreply.github.com>
2024-03-10 10:01:30 +0800

    Update docker_install.sh
    fix docker group

--- linux/docker_install.sh
+++ linux/docker_install.sh
@@ -35,7 +35,12 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo usermod -G docker -a "$USER"
+if [ "$(whoami)" = "root" ]; then
+    sudo usermod -G docker -a user
+else
+    sudo usermod -G docker -a "$USER"
+fi
+
 
 # sudo docker volume create portainer_data
 # sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
