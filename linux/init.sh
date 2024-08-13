57d3fe9a761990632cc49d38d37809ea24c6fe83
codessse <57928832+codessse@users.noreply.github.com>
2024-08-13 13:50:29 +0800

    Update init.sh
    Change username to variable

--- linux/init.sh
+++ linux/init.sh
@@ -6,7 +6,7 @@ apt-get install -y vim ncdu lnav ssh sudo wget curl bat gpg tree git htop dnsuti
 python3-pip
 apt-get autoremove
 
-if ! id -u user >/dev/null 2>&1; then
+if ! id 1000 >/dev/null 2>&1; then
   adduser --disabled-password --gecos "" user
   echo user:" " | chpasswd
   adduser user sudo
@@ -21,21 +21,21 @@ apt-get install -y linux-headers-"$(uname -r)" || apt-get install -y linux-heade
 apt-get upgrade -y
 
 unset DEBIAN_FRONTEND
+username=$( id -u -n 1000 )
 
 ## tmux setting
-
 if [ ! -f  "/usr/share/doc/tmux/example_tmux.conf" ]; then
   echo "tmux example file isn't existed"
   exit 1
 fi
 
-cp /usr/share/doc/tmux/example_tmux.conf /home/user/.tmux.conf
-chown user:user  /home/user/.tmux.conf
+cp /usr/share/doc/tmux/example_tmux.conf "/home/$username/.tmux.conf"
+chown "$username:$username" "/home/$username/.tmux.conf"
 
-sed -i 's/set -g remain-on-exit on/#set -g remain-on-exit on/g'  /home/user/.tmux.conf
-sed -i 's/set -g mouse on/#set -g mouse on/g'  /home/user/.tmux.conf
-sed -i 's/unbind -n MouseDrag1Pane/#unbind -n MouseDrag1Pane/g'  /home/user/.tmux.conf
-sed -i 's/unbind -Tcopy-mode MouseDrag1Pane/#unbind -Tcopy-mode MouseDrag1Pane/g'  /home/user/.tmux.conf
+sed -i 's/set -g remain-on-exit on/#set -g remain-on-exit on/g' "/home/$username/.tmux.conf"
+sed -i 's/set -g mouse on/#set -g mouse on/g' "/home/$username/.tmux.conf"
+sed -i 's/unbind -n MouseDrag1Pane/#unbind -n MouseDrag1Pane/g' "/home/$username/.tmux.conf"
+sed -i 's/unbind -Tcopy-mode MouseDrag1Pane/#unbind -Tcopy-mode MouseDrag1Pane/g' "/home/$username/.tmux.conf"
 
 curl https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux | tee /etc/bash_completion.d/tmux > /dev/null
-cp /home/user/.tmux.conf /root/
+cp "/home/$username/.tmux.conf" /root/
