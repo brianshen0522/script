72e3160f1e072ce494cda6e1b830d9ca6b905b88
codessse <57928832+codessse@users.noreply.github.com>
2025-03-07 17:36:36 +0800

    Update security_policy.sh

--- linux/security_policy.sh
+++ linux/security_policy.sh
@@ -2,7 +2,7 @@
 
 # system log
 echo "Enable ssh log"
-apt install rsyslog
+apt install rsyslog -y
 sed -i 's/#SyslogFacility AUTH/SyslogFacility AUTH/g' /etc/ssh/sshd_config
 sed -i 's/#LogLevel INFO/LogLevel INFO/g' /etc/ssh/sshd_config
 
