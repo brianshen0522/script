4d89a46f0f659cfde97eba28357ca25be742af52
codessse <57928832+codessse@users.noreply.github.com>
2025-03-07 17:40:19 +0800

    Update security_policy.sh
    add sudo command and check .ssh folder

--- linux/security_policy.sh
+++ linux/security_policy.sh
@@ -2,25 +2,28 @@
 
 # system log
 echo "Enable ssh log"
-apt install rsyslog -y
-sed -i 's/#SyslogFacility AUTH/SyslogFacility AUTH/g' /etc/ssh/sshd_config
-sed -i 's/#LogLevel INFO/LogLevel INFO/g' /etc/ssh/sshd_config
+sudo apt install rsyslog -y
+sudo sed -i 's/#SyslogFacility AUTH/SyslogFacility AUTH/g' /etc/ssh/sshd_config
+sudo sed -i 's/#LogLevel INFO/LogLevel INFO/g' /etc/ssh/sshd_config
 
 # Log in with the key only
 echo "Set only key login"
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/g' /etc/ssh/sshd_config
-sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sudo sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/g' /etc/ssh/sshd_config
+sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 
-systemctl restart sshd.service
+[ ! -d "$HOME/.ssh" ] && mkdir "$HOME/.ssh"
+chmod 777 "$HOME/.ssh"
+
+sudo systemctl restart sshd.service
 
 # Setting fail2ban
 echo "Install fail2ban"
-apt install fail2ban -y
-systemctl enable fail2ban
-systemctl start fail2ban
+sudo apt install fail2ban -y
+sudo systemctl enable fail2ban
+sudo systemctl start fail2ban
 
 echo "Set ban rule"
-sed -i 's/#bantime  = 10m/bantime  = 24h/g' /etc/fail2ban/jail.conf
-sed -i 's/#findtime  = 10m/findtime  = 5m/g' /etc/fail2ban/jail.conf
-systemctl reload fail2ban
+sudo sed -i 's/#bantime  = 10m/bantime  = 24h/g' /etc/fail2ban/jail.conf
+sudo sed -i 's/#findtime  = 10m/findtime  = 5m/g' /etc/fail2ban/jail.conf
+sudo systemctl reload fail2ban
